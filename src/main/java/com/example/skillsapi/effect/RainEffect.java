4b3b0309847ecf92c3ff21393c8da9bd2f6f9ea8
faaeeL <faelsuckatosu@gmail.com>
2026-07-16 14:28:55 +0700

    Fix bugs in the RainEffect.java

--- src/main/java/com/example/skillsapi/effect/RainEffect.java
+++ src/main/java/com/example/skillsapi/effect/RainEffect.java
@@ -16,7 +16,7 @@ import org.bukkit.util.Vector;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.ThreadLocalRandom;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 /**
